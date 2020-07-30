b3379b848c35aefc88f8a242f1cfd5cc5ffd7119
jess <jesyka.cheng@gmail.com>
2020-07-30 13:37:16 -0700

    nav bar

--- styles/button.js
+++ styles/button.js
@@ -17,3 +17,15 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
